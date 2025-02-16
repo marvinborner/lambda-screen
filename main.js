5aef4a9cc223cf93361fb80f71fe83eb18577a5f
Marvin Borner <git@marvinborner.de>
2025-02-16 17:51:57 +0100

    Store per vertex

--- main.js
+++ main.js
@@ -67,30 +67,33 @@ in vec2 aOffset;
 in vec4 aColor;
 
 uniform mat3 uProjectionMatrix;
+uniform mat3 uTransformMatrix;
 
-varying vec4 vColor;
+out vec4 vColor;
 
 void main() {
-    vec3 pos = uProjectionMatrix * vec3(aVertexPosition + aOffset, 1.0);
+    vec3 pos = uProjectionMatrix * uTransformMatrix * vec3(aVertexPosition + aOffset, 1.0);
+    // vec3 pos = uProjectionMatrix * uTransformMatrix * vec3(aVertexPosition * vec2(1.0, 1.0) + aOffset, 1.0);
     gl_Position = vec4(pos.xy, 0.0, 1.0);
     vColor = aColor;
 }
 `;
 
 const fragmentShader = `
-    varying vec4 vColor;
-    void main() {
-        gl_FragColor = vColor;
-    }
+in vec4 vColor;
+void main() {
+    gl_FragColor = vColor;
+}
 `;
 
 const shader = PIXI.Shader.from({
   gl: { vertex: vertexShader, fragment: fragmentShader },
 });
 
-const CHUNK_AMOUNT = 5;
+const CHUNK_AMOUNT = 4;
 let cache = {};
 const render = (x1, x2, y1, y2, color, cont) => {
+  // console.log(x1, y1, ",", x2, y2, color);
   const width = x2 - x1;
   const height = y2 - y1;
   console.assert(width == height);
@@ -102,23 +105,25 @@ const render = (x1, x2, y1, y2, color, cont) => {
     colors = elem.colors;
     count = elem.count;
   } else {
-    offsets = new Float32Array(CHUNK_AMOUNT * 4);
-    colors = new Float32Array(CHUNK_AMOUNT * 4);
+    offsets = new Float32Array(CHUNK_AMOUNT * 4 * 2);
+    colors = new Float32Array(CHUNK_AMOUNT * 4 * 4);
     count = { n: 0 };
     cache[width] = { offsets, colors, count };
   }
 
-  offsets[count.n * 4] = x1;
-  offsets[count.n * 4 + 1] = y1;
-  offsets[count.n * 4 + 2] = x1;
-  offsets[count.n * 4 + 3] = y1;
+  const idx = count.n * 8;
+  const colorIdx = count.n * 16;
+  for (let v = 0; v < 4; v++) {
+    offsets[idx + v * 2] = x1;
+    offsets[idx + v * 2 + 1] = y1;
 
-  colors[count.n * 4] = color == "Black";
-  colors[count.n * 4 + 1] = color == "Grey";
-  colors[count.n * 4 + 2] = color == "White";
-  colors[count.n * 4 + 3] = 1.0; // Alpha
+    colors[colorIdx + v * 4] = color == "Black";
+    colors[colorIdx + v * 4 + 1] = color == "Grey";
+    colors[colorIdx + v * 4 + 2] = color == "White";
+    colors[colorIdx + v * 4 + 3] = 1.0;
+  }
 
-  if (count.n++ == CHUNK_AMOUNT) {
+  if (++count.n == CHUNK_AMOUNT) {
     const geometry = new PIXI.Geometry();
     geometry.addAttribute(
       "aVertexPosition",
@@ -135,25 +140,56 @@ const render = (x1, x2, y1, y2, color, cont) => {
       2,
     );
     geometry.addIndex([0, 1, 2, 0, 2, 3]);
-    geometry.addAttribute("aOffset", offsets, 4, false, true);
-    geometry.addAttribute("aColor", colors, 4, false, true);
+
+    const TYPES_FLOAT = 5126; // TODO
+    geometry.addAttribute(
+      "aOffset",
+      offsets.subarray(0, count.n * 8),
+      2,
+      false,
+      TYPES_FLOAT,
+    );
+    geometry.addAttribute(
+      "aColor",
+      colors.subarray(0, count.n * 16),
+      4,
+      false,
+      TYPES_FLOAT,
+    );
+
     const mesh = new PIXI.Mesh({ geometry, shader });
     mesh.drawMode = PIXI.DRAW_MODES.TRIANGLES;
-    // mesh.alpha = 1;
-    // mesh.zIndex = 100;
-    // mesh.blendMode = "overlay";
+    // mesh.instanceCount = count.n;
     app.stage.addChild(mesh);
 
     console.log(app.renderer.gl.getError());
     delete cache[x2 - x1];
-    // count.n = 0;
   }
 };
 
 const flush = () => {
   console.log(cache);
+
+  // draw remaining in chunk
+  // const graphics = new PIXI.Graphics();
+  // Object.entries(cache).forEach(([key, elem]) => {
+  //   for (let i = 0; i < elem.count.n; i++) {
+  //     const x1 = elem.offsets[i * 4];
+  //     const y1 = elem.offsets[i * 4 + 1];
+  //     const x2 = x1 + +key;
+  //     const y2 = y1 + +key;
+  //     const r = elem.colors[i * 4];
+  //     const g = elem.colors[i * 4 + 1];
+  //     const b = elem.colors[i * 4 + 2];
+  //     const a = elem.colors[i * 4 + 3];
+  //     graphics.rect(x1, y1, x2 - x1, y2 - y1);
+  //     graphics.fill([r, g, b, a]);
+  //   }
+  // });
+  // app.stage.addChild(graphics);
   // cache = {};
-  // drawScreen(ctxs, colors);
+
+  // cache = {};
   console.log("DONE!");
 };
 
